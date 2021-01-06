a617d3083bc3d72057826e7aa6b6c95f9f7f7df9
VitorTS <livonor@gmail.com>
2021-01-06 07:03:02 -0200

    remove django

--- deep_kanji_site/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'core/index.html', {'condition': True, 'mylist': [1, 2, 3, 4]})
